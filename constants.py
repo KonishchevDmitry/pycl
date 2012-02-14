bbef97e227805341b9a9bb3e4cfe697a0ae47ee1
Dmitry Konishchev <konishchev@gmail.com>
2012-02-14 13:33:42 +0400

    Time constants

--- constants.py
+++ constants.py
@@ -5,8 +5,18 @@
 MINUTE_SECONDS = 60
 """Number of seconds in minute."""
 
-HOUR_SECONDS = MINUTE_SECONDS * 60
+HOUR_SECONDS = 60 * MINUTE_SECONDS
 """Number of seconds in hour."""
+
+DAY_SECONDS = 24 * HOUR_SECONDS
+"""Number of seconds in day."""
+
+WEEK_SECONDS = 7 * DAY_SECONDS
+"""Number of seconds in week."""
+
+MONTH_SECONDS = 4 * WEEK_SECONDS
+"""Number of seconds in month."""
+
 
 KILOBYTE = 1024
 """Number of bytes in kilobyte."""
