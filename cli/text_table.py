21ef87e55c00126b31787b40657a04df3dedc082
Dmitry Konishchev <konishchev@gmail.com>
2012-06-08 19:28:38 +0400

    max_width header option for TextTable

--- cli/text_table.py
+++ cli/text_table.py
@@ -25,11 +25,21 @@ class TextTable:
 
         headers = copy.deepcopy(headers)
 
+        rows = copy.deepcopy(self.__rows)
+        row_lines = [ 1 ] * len(rows)
+
         first = True
         for header in headers:
             max_len = 0
-            for row in self.__rows:
-                max_len = max(max_len, len(unicode(row.get(header["id"]) or "")))
+            for row_id, row in enumerate(rows):
+                cell = unicode(row[header["id"]]) if header["id"] in row else u""
+                cell_lines = self.__get_cell_lines(cell, max_width = header.get("max_width"))
+                row[header["id"]] = cell_lines
+
+                row_lines[row_id] = max(row_lines[row_id], len(cell_lines))
+
+                for line in cell_lines:
+                    max_len = max(max_len, len(line))
 
             header["hide"] = ( header.get("hide_if_empty", False) and max_len == 0 )
             header["max_len"] = max(max_len, len(header.get("name") or ""))
@@ -45,26 +55,48 @@ class TextTable:
 
         stream.write("\n\n")
 
-        for row in self.__rows:
-            first = True
-            for header in headers:
-                if header["hide"]:
-                    continue
+        for row_id, row in enumerate(rows):
+            for line_id in xrange(0, row_lines[row_id]):
+                for header_id, header in enumerate(headers):
+                    if header["hide"]:
+                        continue
 
-                if first:
-                    first = False
-                else:
-                    stream.write(" " * spacing)
+                    if header_id:
+                        stream.write(" " * spacing)
 
-                value = unicode(row.get(header["id"], ""))
+                    cell_lines = row[header["id"]]
+                    cell_line = cell_lines[line_id] if line_id < len(cell_lines) else ""
 
-                align = header.get("align", "right")
-                if align == "left":
-                    value = value.ljust(header["max_len"])
-                elif align == "center":
-                    value = value.center(header["max_len"])
-                else:
-                    value = value.rjust(header["max_len"])
+                    align = header.get("align", "right")
+                    if align == "left":
+                        cell_line = cell_line.ljust(header["max_len"])
+                    elif align == "center":
+                        cell_line = cell_line.center(header["max_len"])
+                    else:
+                        cell_line = cell_line.rjust(header["max_len"])
 
-                stream.write(value)
-            stream.write("\n")
+                    stream.write(cell_line)
+                stream.write("\n")
+
+
+    def __get_cell_lines(self, cell, max_width = None):
+        """Formats a cell according to header requirements."""
+
+        lines = []
+
+        for source_line in cell.split("\n"):
+            line = u""
+
+            for word in source_line.split(" "):
+                if (
+                    max_width is not None and line and
+                    len(line) + 1 + len(word) > max_width
+                ):
+                    lines.append(line)
+                    line = u""
+
+                line += " " + word
+
+            lines.append(line)
+
+        return lines
